b0dc89e8a2550e9da0c6f421877b55e93818b547
DinDin <peter779701@gmail.com>
2025-01-07 18:54:35 +0800

    [Update] finish week1

--- week1/index.js
+++ week1/index.js
@@ -8,6 +8,10 @@
 
 // 1.1 範例
 const alexAge = 25;
+const alexMembershipID = "GYM2024-12345";
+const isRunningOnTreadmill = true;
+console.log(alexAge,alexMembershipID,isRunningOnTreadmill);
+
 
 // ### 題目二：變數命名練習
 // - 瑜伽團課 - 300 元
@@ -16,28 +20,41 @@ const alexAge = 25;
 // 情境：Alex 這個月的運動預算有 3000 元
 // 請修改以下中文變數名稱，讓他符合變數語意
 
-const 變數一 = 300;
-const 變數二 = 500;
-const 變數三 = 1500;
-const 變數四 = 3000;
+const yogaGroupClass = 300;
+const weightTrainingGroupClass = 500;
+const weightTrainingOneOnOneSession = 1500;
+const alexMonthlyExerciseBudget = 3000;
 
 // ### 題目三：變數計算
 // 呈上題，Alex 想要規劃好自己的運動課程，需符合以下三個條件，請將花費總數計算在 AlexBudget 上，一起幫幫他吧！
 // 條件一：一定至少要買 1 堂重訓團課和瑜伽團課
 // 條件二：瑜伽團課只能一次買 3 堂
 // 條件三：一定要花到 2400 以上，並購買 5 堂課程
+let x = 1, y = 1, z = 1;
+const totalPrice =  weightTrainingGroupClass * x + yogaGroupClass * 3 * y + weightTrainingOneOnOneSession * z;
+let AlexBudget = 0;
 
+if  (totalPrice > 2400 && totalPrice < 3000 &&  (x + 3 * y + z) == 5) {
+  AlexBudget = alexMonthlyExerciseBudget - totalPrice;
+};
 console.log(`Alex 買完課程了，他一共剩下 ${AlexBudget} 元`);
 
 // ### 題目四：線稿圖截圖，看圖宣告變數
 // 請參考資料夾內 q4.webp 圖片
 // 請依照你看到的內容來嘗試設計變數和值（至少 3 個）
+const planTitle = "14 堂組合包方案";
+const planPrice = 2520;
+const planeContentArray = ["包含14 堂課，一堂50分鐘","平均每堂價格：180元","9折"];
 
 // ### 題目五：布林值與變數定義，看是否有用對 const, let
 // 情境：Alex 在往健身房的路上，望向城市的風景，請描述她看到的一切，並宣告變數與賦予值
 // 4-1. Alex 在等紅綠燈，他抬頭看一下現在是紅燈，還有 28 秒綠燈（最多 3 個宣告）
 // 4-2. 目前一起等待的機車有 8 台
 // 4.3. Alex 望向天空，看到天上有 5 朵白雲和 1 顆太陽
+let greenlight = 28;
+const waitingScooter = 8;
+let whiteClould = 3;
+const sun = 1;
 
 // ### 題目六：情境題：簡單變數計算
 // 情境：Alex 每天都會帶著 2000cc 的水壺
@@ -50,6 +67,9 @@ console.log(`Alex 買完課程了，他一共剩下 ${AlexBudget} 元`);
 
 let myWater = 2000; // 水壺容量
 myWater -= 500; // 早上喝了 500cc
+myWater -=800;
+myWater += 1000;
+myWater -= 700;
 
 console.log(`Alex 的水壺還有 ${myWater}cc 的水`);
 
@@ -62,7 +82,11 @@ console.log(`Alex 的水壺還有 ${myWater}cc 的水`);
 
 let totalBill = 0;
 const machineUsePrice = 50;
-
+const groupClasses = 150;
+const machineUsePriceTotal = machineUsePrice * 3;
+const groupClassesTotal =  groupClasses * 2;
+totalBill = machineUsePriceTotal + groupClassesTotal;
+const total = totalBill;
 console.log(
   `Anna 本週器械使用費共 ${machineUsePriceTotal} 元，團體課費用共 ${groupClassesTotal} 元，一共消費金額是 ${total}元`
 );
@@ -77,6 +101,10 @@ a = b + 4;
 a - b;
 b += 1;
 
+//a = 4, b = 1;
+//a被宣告成13, a被宣告成 0+4,  b被宣告成 0+1;
+
+
 // ### 題目九：型別查詢
 // 請不要觀看 console.log，透過註解告知解答每個變數的型別
 let c = 'world';
@@ -88,7 +116,11 @@ let h = f + g;
 
 // 請從以下新增註解，告知上面每行各別是哪些型別
 // a 是 string
-// b 是 ???
+// b 是 number;
+// e 是 string
+// f 是 Bool;
+// g 是 number
+// f 是 number;
 
 // ### 題目十：傳值與傳參考
 // 情境：請依照程式碼告知答案是多少，並在下方用註解方式寫上這五行程式碼做了什麼事
@@ -99,3 +131,6 @@ let numberArr2 = numberArr1;
 numberArr2.push(20);
 numberArr2 = [25, 30, 35];
 console.log(numberArr1, numberArr2);
+
+//ANS: [5, 10, 15] [25, 30, 35]
+//arr2複製arr1的值，arr2新增值20, arr2被宣告為[25,30,35]
